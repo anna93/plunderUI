22d4e7f570d4c364e87b7608ce35cc3f3290dafb
Shekhar Joshi <shekharanna1993@gmail.com>
2015-01-28 11:32:04 +0530

    Disabled carousel on selection of character and string

--- js/plunder.js
+++ js/plunder.js
@@ -1,5 +1,8 @@
-function abc() {
+function moveCarousel() {
     $('#custom_carousel').carousel(2);
     $('#custom_carousel .controls li').addClass("active");
+    $('#custom_carousel .controls li').addClass("carousel-disabled");
+    
+    intializeLoader();
 }
 
